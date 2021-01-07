b712f4f689647743c4a7d2a3e999f470d20d26f2
github-action-benchmark <github@users.noreply.github.com>
2021-01-07 00:55:50 +0000

    add rt_multi_threaded (cargo) benchmark result for e42317b072c5240fe190c0a8708cd24bb4a4eb08

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1609980944861,
+  "lastUpdate": 1609980950079,
   "repoUrl": "https://github.com/phlip9/tokio",
   "entries": {
     "sync_semaphore": [
@@ -157,6 +157,54 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 19705517,
             "range": "± 2140775",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "liaoymxsdl@sina.com",
+            "name": "Rust大闸蟹",
+            "username": "baoyachi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e42317b072c5240fe190c0a8708cd24bb4a4eb08",
+          "message": "chore: remove macro_rules semicolon (#3381)\n\nfix build warning",
+          "timestamp": "2021-01-06T15:46:37+09:00",
+          "tree_id": "afddb1695c354a4f98712745b23363ef96774bc8",
+          "url": "https://github.com/phlip9/tokio/commit/e42317b072c5240fe190c0a8708cd24bb4a4eb08"
+        },
+        "date": 1609980948823,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 180913,
+            "range": "± 43787",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 638194,
+            "range": "± 156339",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 5076414,
+            "range": "± 1317422",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 19722574,
+            "range": "± 3111800",
             "unit": "ns/iter"
           }
         ]
